71ee3b9f2592515d8f5eb91411c306d08456f50d
Axel Bock <axel.bock@dvag.com>
2014-05-05 09:31:36 +0200

    upd: added some tests

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+require 'rubygems'
+require 'puppetlabs_spec_helper/module_spec_helper'
+
+
+RSpec.configure do |c|
+
+  # Use color in STDOUT
+  c.color_enabled = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  c.tty = true
+
+  # Use the specified formatter
+  c.formatter = :documentation # :progress, :html, :textmate
+end
