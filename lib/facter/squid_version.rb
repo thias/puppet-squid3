7000d760e0ae1af71586b8ba23cdc286d7aaf98f
Brian Hechinger <bhechinger@cardiomems.com>
2014-11-07 14:14:06 -0500

    fixed to output just version, not all the other crud

--- lib/facter/squid_version.rb
+++ lib/facter/squid_version.rb
@@ -1,5 +1,5 @@
 Facter.add('squid_version') do
   setcode do
-    Facter::Util::Resolution.exec('/usr/sbin/squid3 -v');
+    Facter::Util::Resolution.exec('/usr/sbin/squid3 -v | awk \'/Version/ { print $NF}\'');
   end
 end
