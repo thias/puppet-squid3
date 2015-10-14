c7c15283406b35bfaad940b789c0e836c4b6ea6f
Matthias Saou <matthias@saou.eu>
2015-10-14 18:36:46 +0200

    Minor cosmetic changes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,18 +1,18 @@
-2014-07-15 - 0.2.3
+#### 2014-07-15 - 0.2.3
 * Add refresh_patterns config option (#7, @adamgraves85).
 
-2014-05-12 - 0.2.2
+#### 2014-05-12 - 0.2.2
 * Include short template and allow using custom templates (#5, @flypenguin).
 
-2014-03-28 - 0.2.1
+#### 2014-03-28 - 0.2.1
 * Fix coredump_dir on Debian/Ubuntu (#6, @jinnko).
 
-2013-09-09 - 0.2.0
+#### 2013-09-09 - 0.2.0
 * Add params, start supporting Debian OS family.
 * Automatically pick the right package name on RHEL5.
 * Added maximum_object_size{,_in_memory} parameters (Tristan Helmich).
 
-2013-05-24 - 0.1.1
+#### 2013-05-24 - 0.1.1
 * Add ChangeLog and update Modulefile.
 * Add LICENSE file.
 * Update README and use markdown.
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,44 +1,21 @@
 # Class: squid3
 #
-# Squid 3.x proxy server.
-#
-# Sample Usage :
-#     include squid3
-#
-#     class { 'squid3':
-#       acl => [
-#         'de myip 192.168.1.1',
-#         'fr myip 192.168.1.2',
-#         'office src 10.0.0.0/24',
-#       ],
-#       http_access => [
-#         'allow office',
-#       ],
-#       cache => [ 'deny all' ],
-#       via => 'off',
-#       tcp_outgoing_address => [
-#         '192.168.1.1 country_de',
-#         '192.168.1.2 country_fr',
-#       ],
-#       server_persistent_connections => 'off',
-#     }
-#
 class squid3 (
   # Options are in the same order they appear in squid.conf
-  $use_deprecated_opts  = true,
-  $http_port            = [ '3128' ],
-  $https_port           = [],
-  $acl                  = [],
-  $http_access          = [],
-  $icp_access           = [],
-  $tcp_outgoing_address = [],
-  $cache_mem            = '256 MB',
-  $cache_dir            = [],
-  $cache                = [],
-  $via                  = 'on',
-  $ignore_expect_100    = 'off',
-  $cache_mgr            = 'root',
-  $forwarded_for        = 'on',
+  $use_deprecated_opts           = true,
+  $http_port                     = [ '3128' ],
+  $https_port                    = [],
+  $acl                           = [],
+  $http_access                   = [],
+  $icp_access                    = [],
+  $tcp_outgoing_address          = [],
+  $cache_mem                     = '256 MB',
+  $cache_dir                     = [],
+  $cache                         = [],
+  $via                           = 'on',
+  $ignore_expect_100             = 'off',
+  $cache_mgr                     = 'root',
+  $forwarded_for                 = 'on',
   $client_persistent_connections = 'on',
   $server_persistent_connections = 'on',
   $maximum_object_size           = '4096 KB',
@@ -46,7 +23,11 @@ class squid3 (
   $config_hash                   = {},
   $refresh_patterns              = [],
   $template                      = 'long',
-  $version                       = 'installed'
+  $package_version               = 'installed',
+  $package_name                  = $::squid3::params::package_name,
+  $service_ensure                = 'running',
+  $service_enable                = $::squid3::params::service_enable,
+  $service_name                  = $::squid3::params::service_name,
 ) inherits ::squid3::params {
 
   $use_template = $template ? {
@@ -59,13 +40,15 @@ class squid3 (
     fail('$config_hash does not (yet) work with the "long" template!')
   }
 
-
-  package { 'squid3_package': ensure => $version, name => $package_name }
+  package { 'squid3_package':
+    ensure => $package_version,
+    name   => $package_name,
+  }
 
   service { 'squid3_service':
+    ensure    => $service_ensure,
     enable    => $service_enable,
     name      => $service_name,
-    ensure    => running,
     restart   => "service ${service_name} reload",
     path      => ['/sbin', '/usr/sbin'],
     hasstatus => true,
