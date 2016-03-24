74940ea9c9a197115b8f04a4a48d71b67ad3a475
Robert Cashman <cash@simspace.com>
2016-03-24 15:02:25 -0400

    Allow for override of access_log parameter.

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,6 +8,7 @@ class squid3 (
   $acl                           = [],
   $ssl_ports                     = [ '443' ],
   $safe_ports                    = [ '80', '21', '443', '70', '210', '1025-65535', '280', '488', '591', '777', ],
+  $access_log                    = $access_log,
   $http_access                   = [],
   $icp_access                    = [],
   $tcp_outgoing_address          = [],
