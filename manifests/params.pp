8681994a279a71c0eb09c861b53017327bc75abf
Softcom Deploy <softcomdeploy>
2016-06-03 15:57:56 -0700

    Fix logic for Ubuntu >= 16.04

--- manifests/params.pp
+++ manifests/params.pp
@@ -16,29 +16,20 @@ class squid3::params {
       $coredump_dir   = '/var/spool/squid'
     }
     'Debian': {
-      $package_name   = 'squid3'
-      $service_name   = 'squid3'
-      $service_enable = false
-      $config_file    = '/etc/squid3/squid.conf'
-      $log_directory  = '/var/log/squid3'
-      $coredump_dir   = '/var/spool/squid3'
-    }
-    'Ubuntu': {
-      if versioncmp($::operatingsystemrelease,'16.04') < 0 {
-        $package_name = 'squid3'
-        $package_name   = 'squid3'
-        $service_name   = 'squid3'
-        $service_enable = false
-        $config_file    = '/etc/squid3/squid.conf'
-        $log_directory  = '/var/log/squid3'
-        $coredump_dir   = '/var/spool/squid3'
-      } else {
+      if $::operatingsystem == 'Ubuntu' and versioncmp($::operatingsystemrelease,'16.04') >= 0 {
         $package_name   = 'squid'
         $service_name   = 'squid'
         $service_enable = false
         $config_file    = '/etc/squid/squid.conf'
         $log_directory  = '/var/log/squid'
         $coredump_dir   = '/var/spool/squid'
+      } else {
+        $package_name   = 'squid3'
+        $service_name   = 'squid3'
+        $service_enable = false
+        $config_file    = '/etc/squid3/squid.conf'
+        $log_directory  = '/var/log/squid3'
+        $coredump_dir   = '/var/spool/squid3'
       }
     }
     'FreeBSD': {
