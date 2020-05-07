18c00effee4a680eed35a9d512f284e20bddc47b
David Castañeda <edupr91@gmail.com>
2020-05-07 11:44:33 +0200

    Add auth_param

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,7 @@
 #
 class squid3 (
   # Options are in the same order they appear in squid.conf
+  $auth_params                   = [],
   $use_deprecated_opts           = true,
   $use_default_localnet          = true,
   $http_port                     = [ '3128' ],
